6c9ebf84f718f3e244ff0d52e34bddf45c1f0c76
EdoardoRobaItauros <83460842+EdoardoRobaItauros@users.noreply.github.com>
2022-02-16 21:39:38 +0100

    email

--- jenkins/sendEmailWeekly.groovy
+++ jenkins/sendEmailWeekly.groovy
@@ -1,3 +1,4 @@
 stage('Get Post Configuration Request Parameters') {
     println("Hello World!")
+    emailext mimeType:'text/html', body: "Hello World!", subject: 'Resource Provisioning Result', to: "roba.edoardo@gmail.com"
 }
