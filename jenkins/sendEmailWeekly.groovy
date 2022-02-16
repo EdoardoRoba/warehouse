f5889cbdffd0de0c75128a0c5baee57d8b759d3e
EdoardoRobaItauros <83460842+EdoardoRobaItauros@users.noreply.github.com>
2022-02-16 22:22:46 +0100

    fix subject

--- jenkins/sendEmailWeekly.groovy
+++ jenkins/sendEmailWeekly.groovy
@@ -1,4 +1,4 @@
 stage('Get Post Configuration Request Parameters') {
     println("Hello World!")
-    emailext mimeType:'text/html', body: "Hello World!", to: "roba.edoardo@gmail.com"
+    emailext mimeType:'text/html', body: "Hello World!", subject: 'Report settimanale - magazzino', to: "roba.edoardo@gmail.com"
 }
